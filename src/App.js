9643ccc61c37a488bd6242011224457de07bdcb2
Khoa <tndkhoa0903@gmail.com>
2021-09-17 18:57:12 +0700

    feat: expenses section

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenseItems = [
     {
-      id: 'e1',
-      title: 'Toilet Paper',
+      id: "e1",
+      title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: 'e3',
-      title: 'Car Insurance',
+      id: "e3",
+      title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
+      id: "e4",
+      title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ]
-
-  const renderItems = () => {
-    return expenseItems.map((item, index) => {
-     return <ExpenseItem key={index} title={item.title} date={item.date} price={item.price}/>
-    })
-  }
+  ];
 
   return (
-    <div className="App">
-      {renderItems()}
+    <div>
+      <Expenses items={expenseItems} />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,21 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+const Expenses = (props) => {
+  const renderItems = () => {
+    return props.items.map((item, index) => {
+      return (
+        <ExpenseItem
+          key={index}
+          title={item.title}
+          date={item.date}
+          amount={item.amount}
+        />
+      );
+    });
+  };
+
+  return <Card className="expenses">{renderItems()}</Card>;
+};
+
+export default Expenses;
