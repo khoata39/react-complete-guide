60a24cb0e76dc1fc7592be87e930014ca1b02549
Khoa <tndkhoa0903@gmail.com>
2021-10-02 17:06:50 +0700

    add input field

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className='new-expense__controls'>
+        <div class='new-expense__control'>
+          <label>Title</label>
+          <input type='text' />
+        </div>
+
+        <div class='new-expense__control'>
+          <label>Amount</label>
+          <input type='number' min='0.01' step='0.01' />
+        </div>
+
+        <div class='new-expense__control'>
+          <label>Date</label>
+          <input type='date' min='2019-01-01' max='2022-01-01' />
+        </div>
+
+        <div className='new-expense__actions'>
+          <button type='submit'>Add expense</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
